bad1988a6f7d7b58ba665697154c41ab28ab663a
ericccccccccccccc2 <113203325+ericccccccccccccc2@users.noreply.github.com>
2023-03-07 14:02:39 -0500

    asdafasd

---
+++ poopy/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input v-model="text" />
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+    };
+  },
+  methods: {
+    wowie() {
+      console.log("amongus");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

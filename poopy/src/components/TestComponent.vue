72d409b51804abbf7a0cb2386393c0a802a3ee27
ericccccccccccccc2 <113203325+ericccccccccccccc2@users.noreply.github.com>
2023-03-14 14:00:17 -0400

    i made progress today :+1:

---
+++ poopy/src/components/TestComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3><slot name="title"></slot></h3>
+    <slot name="img"></slot>
+
+    <h4><slot name="price"></slot></h4>
+    <button>buy this item</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+div {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  height: 300px;
+  width: 300px;
+}
+
+img {
+  height: 20px;
+  width: 30px;
+}
+</style>
--- poopy/src/views/AboutView.vue
+++ poopy/src/views/AboutView.vue
@@ -1,6 +1,21 @@
+<script setup>
+import WelcomeItemVue from "../components/WelcomeItem.vue";
+import TheWelcome from "../components/TheWelcome.vue";
+import TestComponent from "../components/TestComponent.vue";
+</script>
+
 <template>
   <div class="about">
+    <TestComponent>
+      <template #title> Amongus </template>
+      <template #img>
+        <img
+          src="https://assets.nintendo.com/image/upload/f_auto/q_auto/dpr_2.0/c_scale,w_400/ncom/en_US/games/switch/a/among-us-switch/description-image"
+        />
+      </template>
+    </TestComponent>
     <h1>This is an about page</h1>
+    <WelcomeItemVue></WelcomeItemVue>
   </div>
 </template>
 
@@ -11,5 +26,10 @@
     display: flex;
     align-items: center;
   }
+}
+
+img {
+  height: 200px;
+  width: 150px;
 }
 </style>
